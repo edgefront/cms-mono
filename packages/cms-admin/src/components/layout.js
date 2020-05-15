6d536b2035fdd09563a43f66e81f038c50f90695
Daniel Liu <hnnk@qq.com>
2020-05-15 11:21:55 +0800

    put app basic structure to layout folder

---
+++ packages/cms-admin/src/components/layout.js
@@ -0,0 +1,65 @@
+import { innerHTML } from '../helper'
+import React from 'react';
+import {
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import logo from '../logo.svg';
+
+function Layout(){
+  return (
+    <div className="container">
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
+        <p>
+          Desc
+        </p>
+      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+      <main className="page-container">
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </main>
+      <footer {...innerHTML('&copy; EdgeFront Inc')}></footer>
+    </div>
+  )
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+export { Layout, Layout as default }
