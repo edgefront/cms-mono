24044feb2009bc5ed3a49738a878bd21337760bc
Daniel Liu <hnnk@qq.com>
2020-05-27 10:21:36 +0800

    enhanced knex db, add test service and routes

--- packages/api/src/db/db.ts
+++ packages/api/src/db/db.ts
@@ -3,4 +3,36 @@ import { dbConfig } from './dbConfig'
 
 const db = knex(dbConfig)
 
-export { db, db as default }
+// function getProxyHandler() {
+//   return {
+//     get: function (target, key) {
+//       //proxy through object graph
+//       if (typeof target[key] === 'object' && target[key] !== null){
+//         return new Proxy(target[key], getProxyHandler());
+//       } else {
+//         return target[key];      
+//       }
+//     }
+//   }
+// }
+
+let handler = {
+  get: function(target, key) {
+      console.log('getting '+key);
+      if (target[key]) {
+        console.log('return knex[key] field '+key);
+        return target[key];
+      } else {
+        console.log('calling knex(tableName) function '+key);
+        return target(key);
+      }
+  },
+  set: function(target, key, value) {
+      console.log('setting '+key);
+      target[key] = value;
+      return true;
+  }
+}
+let dbProxy = new Proxy(db, handler)
+
+export { dbProxy as db, dbProxy as default }
---
+++ packages/cms-api/src/routes/test.ts
@@ -0,0 +1,34 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable import/no-named-default */
+import { Next, default as Koa } from 'koa'
+import { TestService } from '../services'
+import {
+  decorators,
+} from '@edgefront/api'
+import { util } from '@edgefront/api'
+
+const {
+  Controller, Get, Autowired, Post,
+} = decorators
+@Controller()
+export default class TestController {
+  @Autowired()
+  public testService: TestService
+
+  @Get('test1')
+  async someGetMethod(ctx: Koa.Context, next: Next): Promise<void> {
+    const result = await this.testService.dbService()
+    ctx.body = `Hello World 1!${JSON.stringify(result)} ${this.testService.testMethod(666)}`
+    await next()
+  }
+
+  @Get('')
+  someGetMethod2(ctx: Koa.Context): void{
+    ctx.body = `Hello World 2!${JSON.stringify(ctx.header)}`
+  }
+
+  @Get('db')
+  async someGetMethod3(ctx: Koa.Context): Promise<void> {
+    ctx.body = await this.testService.dbService()
+  }
+}
